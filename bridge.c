7d88385923ba86dd72fe34bf258c06532733629b
admidere <admasud89@gmail.com>
2022-11-16 07:38:36 -0800

    team simple shell

--- bridge.c
+++ bridge.c
@@ -1,21 +1,21 @@
 #include "shell.h"
 /**
- * bridge - Will check to see whether we are dealing with a builtin or nota
- * @check: Figures out what to execute
- * @args: Arguments passed from cmdline broken up
- * Return: 1 if user entered a path/builtin, 2 if user entered a binary
- */
+* bridge - Will check to see whether we are dealing with a builtin or nota
+* @check: Figures out what to execute
+* @args: Arguments passed from cmdline broken up
+* Return: 1 if user entered a path/builtin, 2 if user entered a binary
+*/
 int bridge(char *check, char **args)
 {
-	int i = 0;
+int i = 0;
 
-	if (builtins_checker(args) == 1)
-		return (1);
-	while (check[i] != '\0')
-	{
-		if (check[i] == '/')
-			return (1);
-		i++;
-	}
-	return (2);
+if (builtins_checker(args) == 1)
+return (1);
+while (check[i] != '\0')
+{
+if (check[i] == '/')
+return (1);
+i++;
+}
+return (2);
 }
